9c8189df898504b0c564ccf3b7f2009526b2fd6f
vidwalk <claudiurediu@gmail.com>
2020-11-25 11:41:09 +0100

    Pdf to Text script

---
+++ pdfToText.py
@@ -0,0 +1,29 @@
+import fitz  # this is pymupdf
+import os
+
+
+def findPdfs(path):
+    result = list()
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.pdf'):
+                result.append(file)
+    return result
+
+
+def writeToText(path, pdfName):
+    with fitz.open(path+pdfName) as doc:
+        text = ""
+        for page in doc:
+            text += page.getText()
+
+    text_file = open(
+        "TextFiles/" + pdfName[:-4]+".txt", "w+", encoding="utf-8")
+    text_file.write(text)
+    text_file.close()
+
+
+if __name__ == "__main__":
+    path = ""
+    for pdfs in findPdfs(path):
+        writeToText(path, pdfs)
